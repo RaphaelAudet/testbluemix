ec23c8f55f47936c3a0ff085da15c4366c6e3a9e
Raphael Audet <raphael.audet@mendix.com>
2017-02-08 16:29:50 +0100

    updated ibm lib to 3.5.3
    Signed-off-by: Raphael Audet <raphael.audet@mendix.com>

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,17 +1,15 @@
 
-//import com.ibm.watson.developer_cloud.language_translation.*;
-import com.ibm.watson.developer_cloud.language_translation.v2.LanguageTranslation;
-import com.ibm.watson.developer_cloud.language_translation.v2.model.IdentifiableLanguage;
+import com.ibm.watson.developer_cloud.language_translator.v2.LanguageTranslator;
+import com.ibm.watson.developer_cloud.language_translator.v2.model.IdentifiableLanguage;
 
 import java.util.List;
 
 public class Application {
 
     public static void main(String[] args){
-        String username=  "myusername";
-        String password = "mypassword";
-        com.ibm.watson.developer_cloud.language_translation.v2.LanguageTranslation service = new LanguageTranslation();
-//        LanguageTranslator service = new LanguageTranslator();
+        String username=  "username";
+        String password = "password";
+        LanguageTranslator service = new LanguageTranslator();
         service.setUsernameAndPassword(username,password);
         List<IdentifiableLanguage> identifieableLanguages;
         identifieableLanguages = service.getIdentifiableLanguages().execute();
